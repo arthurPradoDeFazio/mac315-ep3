09f303cc28554cbbcebf9b9e4c4546847083f780
Arthur Prado De Fazio <arthur.fazio@usp.br>
2022-12-07 18:42:27 -0300

    Examples

--- revised_simplex.m
+++ revised_simplex.m
@@ -45,8 +45,9 @@ function [Binv Binv_A bind] = drive_artificial_out(l, Binv_A, bind, Binv, m_aux)
   bind(l) = j;
 endfunction
 
-function [A b Binv Binv_A bind m_aux n_aux] = remove_redundant_constraint(l, A, Binv, bind, m_aux, n_aux)
-  printf("\nA restrićão %d é redundante e será removida\n");
+function [A b Binv Binv_A bind m_aux n_aux] = remove_redundant_constraint(l, A, b, Binv, bind, m_aux, n_aux)
+  printf("\nA restrićão %d é redundante e será removida\n", l);
+  A(l, :)
   A(l, :) = [];
   b(l) = [];
   Binv(l, :) = [];
@@ -74,12 +75,11 @@ function [A b bind Binv] = drive_artificials_out(A, b, bind, Binv, m_aux, n_aux)
       Binv_A
       bind
       if all(Binv_A(l, :) == 0)
-        [A Binv Binv_A bind m_aux n_aux] = remove_redundant_constraint(l, A, b, Binv, bind, m_aux, n_aux);
+        [A b Binv Binv_A bind m_aux n_aux] = remove_redundant_constraint(l, A, b, Binv, bind, m_aux, n_aux);
       else
         [Binv Binv_A bind] = drive_artificial_out(l, Binv_A, bind, Binv, m_aux);
         l += 1;
       endif
-      
     else
       l += 1;
     endif
@@ -104,6 +104,7 @@ function [opt_cost v bind Binv A b] = phaseI(A, b, c, m, n)
     endif
     
     [A b bind Binv] = drive_artificials_out(A, b, bind, Binv, m_aux, n_aux);
+    
     v(n + 1:end) = [];
 endfunction
 
@@ -202,7 +203,10 @@ function x = update_basic_solution(x, bind, m, u, theta, j, l)
     Binv(l, :) = Binv(l, :) / (u(l));
  endfunction
 
-
+function [opt_cost v] = phaseII(A, b, c, x, bind, Binv, m, n)
+  printf("\nFase 2:\n");
+  [opt_cost v bind Binv] = simplex_helper(A, b, c, m, n, x, bind, Binv);
+endfunction
 % ========================================================================================================================
 % Main function: 
 % ========================================================================================================================
@@ -252,14 +256,21 @@ endfunction
 
 
 function [ind x d] = simplex(A, b, c, m, n)
-  [opt_cost v bind Binv A] = phaseI(A, b, c, m, n);
+  [opt_cost v bind Binv A b] = phaseI(A, b, c, m, n);
   
   if opt_cost > 0
     [ind x d] = infeasible_problem(opt_cost);
     return
   endif
   
+  [opt_cost v] = phaseII(A, b, c, v, bind, Binv, size(A)(1), n);
   
+  x = d = v;
+  if opt_cost == -Inf
+    ind = -1;
+  else
+    ind = 0;
+  endif
 endfunction
 
 % ========================================================================================================================
@@ -283,15 +294,23 @@ b = [10 3]';
 c = [4  5  1 -1 -1]';
 m = 2;
 n = 5;
-x = [10 0 3 0 0]';
-bind = [1 3];
-B = [ [1; 0] [0; 1] ];
-Binv = inv(B);
-
-%[A_aux b_aux c_aux m_aux n_aux] = auxiliary_problem(A, b, c, m, n);
-%initial_solution_to_auxiliary_problem(A_aux, b_aux, c_aux, m_aux, n_aux);
 
 
-introduce_artificial_variables(A, b, c, m, n);
 
-%[ind v] = phaseII(A,b,c,m,n,x,bind, Binv);
+% problem with optimal solution and redundant constraint
+%A = [[1; -1; 0; 0] [2; 2; 4; 0] [3; 6; 9; 3] [0;0;0; 1]] % pagina 114 do livro
+%b = [3 2 5 1]'
+%c = [1 1 1 0]'
+%m = 4
+%n = 4
+
+% Infeasible problem
+%A = [[3; 8; 1; 12] [4; 7; 1; 12] [7; 90; 1; 98] [9; 1; 1; 11]]
+%b = [5 2 10 16]' 
+%c = [0 0 0 0]'
+%m = 4
+%n = 4
+
+simplex(A, b, c, m, n)
+
+
